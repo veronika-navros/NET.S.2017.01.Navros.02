db3b00647b9860d452a2473744eb11a060b4e96c
valhallla <kruner.kruner@gmail.com>
2017-03-13 00:28:41 +0300

    no message

--- Task1.CUI/Program.cs
+++ Task1.CUI/Program.cs
@@ -7,6 +7,7 @@ namespace Task1.CUI
     {
         static void Main()
         {
+            //Input
             Write("Enter array of integers: ");
             string enterString = ReadLine();
             if (enterString != null)
@@ -14,6 +15,7 @@ namespace Task1.CUI
                 string[] stringsArray = enterString.Split(' ');
                 int[] array = new int[stringsArray.Length];
 
+                //Parsing entered string into integer array
                 try
                 {
                     for (int i = 0; i < stringsArray.Length; i++)
@@ -29,8 +31,10 @@ namespace Task1.CUI
                     return;
                 }
 
+                //Sorting the array
                 array = MergeSort.Sort(array);
 
+                //Output
                 Clear();
                 WriteLine("Sorted array:");
                 foreach (int element in array)
--- Task1/MergeSort.cs
+++ Task1/MergeSort.cs
@@ -3,10 +3,25 @@ using System.Linq;
 
 namespace Task1
 {
+    /// <summary>
+    /// Implements the merge sort method for one-dimensional integer array
+    /// </summary>
     public class MergeSort
     {
+        /// <summary>
+        /// Merges two sorted one-dimensional integer arrays into one 
+        /// </summary>
+        /// <param name="array1">The first one-dimensional integer array to merge</param>
+        /// <param name="array2">The second one-dimensional integer array to merge</param>
+        /// <returns>Sorted one-dimensional integer array consisting of elements of two source arrays</returns>
+        /// <exception cref="ArgumentNullException"></exception>
         static int[] Merge(int[] array1, int[] array2)
         {
+            if (array1 == null || array2 == null)
+            {
+                throw new ArgumentNullException();
+            }
+
             int currentIndex1 = 0, currentIndex2 = 0;
             int[] mergedArray = new int[array1.Length + array2.Length];
 
@@ -35,6 +50,12 @@ namespace Task1
             return mergedArray;
         }
 
+        /// <summary>
+        /// Sorts the specified one-dimensional iteger array
+        /// </summary>
+        /// <param name="array">One-dimensional integer array for sorting</param>
+        /// <returns> The sorted one-dimensional integer array </returns>
+        /// <exception cref="ArgumentNullException"></exception>
         public static int[] Sort(int[] array)
         {
             if (array == null)
