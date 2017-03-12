ac15d9e68f927c11c07c97016fa4c212a36da139
valhallla <kruner.kruner@gmail.com>
2017-03-12 16:00:01 +0300

    no message

---
+++ Task1.CUI/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using static System.Console;
+
+namespace Task1.CUI
+{
+    class Program
+    {
+        static void Main()
+        {
+            Write("Enter array of integers: ");
+            string enterString = ReadLine();
+            if (enterString != null)
+            {
+                string[] stringsArray = enterString.Split(' ');
+                int[] array = new int[stringsArray.Length];
+
+                try
+                {
+                    for (int i = 0; i < stringsArray.Length; i++)
+                    {
+                        array[i] = int.Parse(stringsArray[i]);
+                    }
+                }
+                catch (FormatException)
+                {
+                    Clear();
+                    WriteLine("A non-numeric character found");
+                    ReadKey();
+                    return;
+                }
+
+                array = MergeSort.Sort(array);
+
+                Clear();
+                WriteLine("Sorted array:");
+                foreach (int element in array)
+                {
+                    Write(element + " ");
+                }
+                ReadKey();
+            }
+        }
+    }
+}
---
+++ Task1/MergeSort.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace Task1
+{
+    public class MergeSort
+    {
+        static int[] Merge(int[] array1, int[] array2)
+        {
+            int currentIndex1 = 0, currentIndex2 = 0;
+            int[] mergedArray = new int[array1.Length + array2.Length];
+
+            for (int i = 0; i < mergedArray.Length; i++)
+            {
+                if (currentIndex2 < array2.Length && currentIndex1 < array1.Length)
+                {
+                    if (array1[currentIndex1] > array2[currentIndex2])
+                    {
+                        mergedArray[i] = array2[currentIndex2++];
+                    }
+                    else
+                    {
+                        mergedArray[i] = array1[currentIndex1++];
+                    }
+                }
+                else if (currentIndex2 < array2.Length)
+                {
+                    mergedArray[i] = array2[currentIndex2++];
+                }
+                else
+                {
+                    mergedArray[i] = array1[currentIndex1++];
+                }
+            }
+            return mergedArray;
+        }
+
+        public static int[] Sort(int[] array)
+        {
+            if (array == null)
+                throw new ArgumentNullException();
+
+            if (array.Length == 1)
+                return array;
+
+            int middlePoint = array.Length / 2;
+            return Merge(Sort(array.Take(middlePoint).ToArray()), Sort(array.Skip(middlePoint).ToArray()));
+        }
+    }
+}
